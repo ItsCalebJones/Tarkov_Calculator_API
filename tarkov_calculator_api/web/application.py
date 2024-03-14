087e7ef8ae4dc37b6e81bb665e461ee378fd4c63
Caleb Jones <ca.jones9119@gmail.com>
2024-03-14 09:03:26 -0400

    feat: add cors config

--- tarkov_calculator_api/web/application.py
+++ tarkov_calculator_api/web/application.py
@@ -2,6 +2,7 @@ from importlib import metadata
 from pathlib import Path
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import UJSONResponse
 from fastapi.staticfiles import StaticFiles
 
@@ -29,6 +30,16 @@ def get_app() -> FastAPI:
         redoc_url=None,
         openapi_url="/api/openapi.json",
         default_response_class=UJSONResponse,
+    )
+
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
     )
 
     # Adds startup and shutdown events.
